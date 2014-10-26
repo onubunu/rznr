95ec0ec263be8d900f77b89c7213b1c2dffc7efb
onubunu <onubunu@hotmail.com>
2014-10-26 10:42:10 +0100

    hazir

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,6 @@
 class JobsController < ApplicationController
-  before_filter :authenticate_user!#, except: [ :index, :show ]
-  before_filter :is_admin?#, except: [ :index, :show ]
+  before_filter :authenticate_user!, except: [ :index ]
+  before_filter :is_admin?, except: [ :index ]
   
   # GET /jobs
   # GET /jobs.json
